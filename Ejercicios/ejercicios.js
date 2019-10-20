8056a00247e08466e14b89a87662574798e8b0bb
IagoCardezo2 <iago.cardezo2@gmail.com>
2019-10-20 18:59:30 +0200

    Añadidos los 3 ejercicios

---
+++ Ejercicios/ejercicios.js
@@ -0,0 +1,69 @@
+"use strict";
+
+// ############################
+// ##Ejercicio 1. CALCULADORA##
+// ############################
+// function Calculadora(num1, num2) {
+//   let operacion = prompt("suma:+ , resta -, multiplicación:*, potencia: **");
+
+//   switch (operacion) {
+//     case "+":
+//       return +num1 + +num2;
+//       break;
+//     case "-":
+//       return num1 - num2;
+//       break;
+//     case "*":
+//       return num1 * num2;
+//       break;
+//     case "**":
+//       return num1 ** num2;
+//       break;
+//     default:
+//       console.log("No se reconoce ésta operación");
+//   }
+// }
+
+// console.log(Calculadora(prompt("num1"), prompt("num2")));
+
+// ######################
+// ##Ejercicio 2. DADO.##
+// ######################
+
+// function lanzarDado() {
+//   let totalScore = 0;
+
+//   while (totalScore < 50) {
+//     let dado = Math.floor(Math.random() * 6 + 1);
+//     totalScore = totalScore + dado;
+//     console.log(`Lanzamiento: ${dado} ; Total ${totalScore}`);
+//   }
+//   return console.log("GAME OVER");
+// }
+
+// lanzarDado();
+
+// ##########################
+// ##Ejercicio 3. REGISTRO.##
+// ##########################
+
+// class person {
+//   constructor (name, age , gender){
+//       this.name = name;
+//       this.age = age;
+//       this.gender= gender;
+//   }
+// }
+
+//  class profesor extends person{
+//      constructor (name, age, gender, subject,group){
+//          super (name,age gender);
+//          this.subject = subject;
+//          this.listaEstudiantes = [];
+//      }
+//  }
+
+//  class student extends person {
+//      constructor (name, age ,gender,subject, group)
+//  }
+
